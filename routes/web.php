16d51a14e1aa18b865e8a2a2405784ab4f1d7ab7
Pablo Rosales <rosalesjuarezpablojesus@gmail.com>
2024-08-27 23:57:42 -0600

    Prepare the dash

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', fn() => redirect('/dashboard'));
 
 Route::middleware([
     'auth:sanctum',
